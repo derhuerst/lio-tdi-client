3c4d126479eea176c8f6a2bf724d2e6ceb19d727
Jannis R <mail@jannisr.de>
2020-07-25 23:01:00 +0200

    decode pbf [todo]

--- example.js
+++ example.js
@@ -7,8 +7,9 @@ const client = createClient('https://tdi.swu.de/tdinterface/')
 ;(async () => {
 	console.log('version', await client.version())
 
-	const stops = await client.stops()
-	stops.pipe(process.stdout)
+	// const stops = await client.stops()
+	// stops.pipe(process.stdout)
+	// todo
 })()
 .catch((err) => {
 	console.error(err)
