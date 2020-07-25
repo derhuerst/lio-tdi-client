f004ef2e3229674a7f4ea8160faeee872aa886da
Jannis R <mail@jannisr.de>
2020-07-25 23:04:36 +0200

    fix build

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# ../bernhackt2018-aldente-backend/proto/tdi
-# todo: store .proto files in this repo
-basedir="$(dirname $(dirname $(realpath "$0")))/bernhackt2018-aldente-backend/proto/tdi"
+set -e
+set -x
+
+cd "$(dirname $(realpath "$0"))"
+
+basedir="bernhackt2018-aldente-backend/proto/tdi"
 
 pbf "$basedir/VersionTdiArray.proto" >pbf/VersionTdiArray.js
 
